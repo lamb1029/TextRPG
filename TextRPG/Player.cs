db159951e3c4580d6b8ba38ce066f0ae7b7866be
lamb1029 <90374849+lamb1029@users.noreply.github.com>
2021-11-29 14:56:37 +0900

    Player Date

---
+++ TextRPG/Player.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextRPG
+{
+    enum PlayerType
+    {
+        None,
+        Knight,
+        Archer,
+        Mage
+    }
+    class Player
+    {
+        protected PlayerType type = PlayerType.None;
+        protected int hp = 0, attack = 0;
+
+        protected Player(PlayerType type)
+        {
+            this.type = type;
+        }
+        public void Setinfo(int hp, int attack)
+        {
+            this.hp = hp;
+            this.attack = attack;
+        }
+        public int GetHP() { return hp; }
+        public int GetAttack() { return attack; }
+    }
+
+    class Knight : Player
+    {
+        public Knight() : base(PlayerType.Knight)
+        {
+            Setinfo(100, 10);
+        }
+    }
+
+    class Archer : Player
+    {
+        public Archer() : base(PlayerType.Archer)
+        {
+            Setinfo(75, 15);
+        }
+    }
+
+    class Mage : Player
+    {
+        public Mage() : base(PlayerType.Mage)
+        {
+            Setinfo(50, 20);
+        }
+    }
+}
